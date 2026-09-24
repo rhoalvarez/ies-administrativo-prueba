36bec96e35fa0d5fb1b18d774092dc859cddea54
rhoalvarez <alvarezrho@gmail.com>
2026-09-23 22:52:10 -0300

    Ajustar vite para verce

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/ies-administrativo-prueba/",
+  base: process.env.VERCEL ? "/" : "/ies-administrativo-prueba/",
   plugins: [react()],
 });
